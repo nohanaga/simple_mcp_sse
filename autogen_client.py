78d36782768b4e36daea6d841bc6011c53d63800
nohanaga <nohanaga@microsoft.com>
2025-04-23 22:22:14 +0900

    fixed

--- autogen_client.py
+++ autogen_client.py
@@ -3,10 +3,6 @@ import asyncio
 from dotenv import load_dotenv
 from autogen_ext.tools.mcp import SseServerParams, mcp_server_tools
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.conditions import MaxMessageTermination, TextMentionTermination, TimeoutTermination
-from autogen_agentchat.messages import AgentEvent, ChatMessage
-from autogen_agentchat.teams import SelectorGroupChat
-from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient, AzureOpenAIChatCompletionClient
 
 async def main() -> None:
